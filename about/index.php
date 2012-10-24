bc4afafe439c840df348888dc78777229327f4f2
Andy Gebby <agebby@windstream.net>
2012-10-23 23:20:21 -0400

    about

--- about/index.php
+++ about/index.php
@@ -9,16 +9,47 @@ include("../include/meta.php");
 <?php include("../include/header.php") ?>
 
 <div class="pagecontent">
-	<div id="homeintro" class="clearfix">
-  
-			<h1>About Us</h1>
-			
-    
-  	</div>
-  
+	<div id="submaincontent" class="clearfix">
 
+		<h1>About Us</h1>
+		<div class="row-fluid">
+			
+			<div class="span8">
+				<p>
+				 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quis odio et justo congue 
+				 aliquet. Mauris dapibus turpis id metus tempor pellentesque. Mauris ultricies, dolor 
+				 sit amet fermentum posuere, felis risus fringilla dolor, ac posuere lectus nibh eget est.
+				  Sed vitae tortor ut felis fringilla tincidunt. Sed porta turpis ac magna viverra eu commodo 
+				  metus luctus. Phasellus euismod placerat scelerisque. Maecenas feugiat lobortis leo, 
+				  ultricies malesuada leo pharetra ut. Donec lacinia tortor eget lorem dignissim in semper 
+				  quam sagittis. Nullam accumsan hendrerit eros quis accumsan.
+				</p>
+
+				<p>
+				Proin semper orci in dolor sagittis ornare imperdiet enim volutpat. Curabitur sit 
+				amet felis lorem, eget auctor libero. Lorem ipsum dolor sit amet, consectetur 
+				adipiscing elit. Nullam ac felis ac lacus condimentum fermentum in id lacus. In hac 
+				habitasse platea dictumst. Suspendisse viverra nisl nec urna varius adipiscing vitae at 
+				tellus. Proin non arcu et dui vulputate euismod. Donec aliquet, lorem sed accumsan adipiscing, 
+				risus lorem varius mauris, non consequat augue felis in arcu. Morbi interdum nibh quis 
+				eros ornare elementum. Suspendisse sit amet nisl lectus, vitae sollicitudin elit.
+				</p>
+
+				<p>
+				Vestibulum scelerisque pulvinar lacus at fringilla. Donec quis condimentum massa. 
+				Proin ultrices aliquam tortor, ut fringilla nunc fermentum vitae. Integer dictum tempus 
+				facilisis. Integer a justo vel dolor tempus dignissim. Praesent sed leo est. Ut egestas 
+				erat eu justo malesuada gravida.
+				</p>
+			</div>
+			<div class="span4">
+				
+			</div>
+		</div>
+		
+
+	</div>
 	<?php include("../include/latestvideos.php") ?>
-	
 </div>
 
 <?php include("../include/ads.php") ?>
