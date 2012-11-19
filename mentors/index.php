a44d00043603da94d9665dda7170195fbb949346
Andy Gebby <agebby@windstream.net>
2012-11-19 12:48:19 -0500

    mentors page

--- mentors/index.php
+++ mentors/index.php
@@ -9,16 +9,57 @@ include("../include/meta.php");
 <?php include("../include/header.php") ?>
 
 <div class="pagecontent">
-	<div id="homeintro" class="clearfix">
-  
-			<h1>Mentors</h1>
-			
-    
-  	</div>
-  
+	<div id="submaincontent" class="clearfix">
+		<h1>Meet the Team</h1>
 
-	<?php include("../include/latestvideos.php") ?>
-	
+		<div id="web_workshops" class="workshopcontainer">
+			<ul class="workshopList">
+				<li>					
+					<div class="mentorInfo">
+						<div class="workshopTile"><a href="/workshops/tutorial.php"><img src="/assets/img/nicole_capuana.jpg" /></a></div>
+						<h2><a href="/workshops/tutorial.php">Nicole Capuana</a></h2>						
+						<div class="mentorQuote">"I mentor to show how UX is a combination of solving puzzles, creating art, and bringing humanity to technology - that it is fun, challenging, and not just for nerds. I am able to do all the things I like to do and still be artistic." -Nicole</div>
+						<div class="mentorDescription">
+							Capuana is Program Officer for HER Ideas in Motion. A seasoned UX professional, Capuana loves the challenge of finding the right balance of information and design that allow people to interact and complete their goals through technology - if she's done her job well you would never know because you would say 'well that was easy'. Follow Nicole on Twitter
+							<a href="https://twitter.com/ncapuana" target="_blank">@ncapuana</a>.
+						</div>
+					</div>
+					<div class="memberEmail">
+						<a href="#">nicole [@] taxibamboo.com</a>
+					</div>					
+				</li>
+
+				<li>					
+					<div class="mentorInfo">
+						<div class="workshopTile"><a href="/workshops/tutorial.php"><img src="/assets/img/maya_lindenmuth.jpg" /></a></div>
+						<h2><a href="/workshops/tutorial.php">Maya Lindenmuth</a></h2>						
+						<div class="mentorQuote">"I want to be able to make a difference in other people's lives, and encourage other girls to follow the road less taken." -Maya</div>
+						<div class="mentorDescription">
+							Lindenmuth is Mentor Outreach Officer for HER Ideas in Motion. She is a Manager of Interface Development at Rosetta.
+						</div>
+					</div>
+					<div class="memberEmail">
+						<a href="#">meamaya [@] gmail.com</a>
+					</div>					
+				</li>
+
+				<li>					
+					<div class="mentorInfo">
+						<div class="workshopTile"><a href="/workshops/tutorial.php"><img src="/assets/img/rachel_wilkins_patel.jpg" /></a></div>
+						<h2><a href="/workshops/tutorial.php">Rachel Wilkins Patel</a></h2>						
+						<div class="mentorQuote">"Learning to write my own code and make my own media was like learning to ride a bike for the first time, it meant freedom to go and create anything anywhere. I mentor as an extension of my professional career and to share this sense of freedom with others." -Rachel</div>
+						<div class="mentorDescription">
+							Wilkins Patel is the founder and director of HER Ideas in Motion and a technologist who is interested in using her skills for social change. With professional experience in web development, web marketing and new media production, she has lived and traveled extensively across four continents and holds a M.F.A. and B.A. from Hollins University. Follow Rachel on Twitter
+							<a href="https://twitter.com/clevelandrachel" target="_blank">@ClevelandRachel</a>.
+						</div>
+					</div>
+					<div class="memberEmail">
+						<a href="#">rachel [@] rachelwilkins.com</a>
+					</div>				
+				</li>
+			</ul>
+		</div>
+	</div>
 </div>
 
 <?php include("../include/ads.php") ?>
