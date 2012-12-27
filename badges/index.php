3e9e34acc7f2fce4f4b4a541f15141fa338cca1c
Andy Gebby <agebby@windstream.net>
2012-12-27 15:54:00 -0500

    fixes for badges and tutorials for css styling

--- badges/index.php
+++ badges/index.php
@@ -14,15 +14,15 @@ include("../include/meta.php");
 		<h1>Badges</h1>
 		<p>Click any of the badges below for information on how to earn those badges!</p>
 
-     	<div id="web_workshops" class="workshopcontainer">
+     	<div id="web_badges" class="badgecontainer">
 			<h2>Web</h2>
-			<ul class="workshopList">
+			<ul class="badgeList">
 				<li>
-					<div class="workshopTile"><a href="/tutorials/tutorial.php"><img src="/assets/img/badge_thumb.jpg" /></a></div>
-					<div class="workshopInfo">
+					<div class="badgeTile"><a href="/tutorials/tutorial.php"><img src="/assets/img/badge_thumb.jpg" /></a></div>
+					<div class="badgeInfo">
 						<h2><a href="/tutorials/tutorial.php">Build AYummy Hamburger With HTML</a></h2>
 						<a href="/tutorials/tutorial.php">Get this Badge!</p></a>
-						<div class="workshopCareer">
+						<div class="badgeCareer">
 							<p>
 								<span class="careerArea">
 								  Career Area
@@ -34,11 +34,11 @@ include("../include/meta.php");
 				</li>
 
 				<li>
-					<div class="workshopTile"><a href="/tutorials/tutorial.php"><img src="/assets/img/badge_thumb.jpg" /></a></div>
-					<div class="workshopInfo">
+					<div class="badgeTile"><a href="/tutorials/tutorial.php"><img src="/assets/img/badge_thumb.jpg" /></a></div>
+					<div class="badgeInfo">
 						<h2><a href="/tutorials/tutorial.php">Design Your Shoe Collection</a></h2>
 						<a href="/tutorials/tutorial.php">Get this Badge!</p></a>
-						<div class="workshopCareer">
+						<div class="badgeCareer">
 							<p>
 								<span class="careerArea">
 								  Career Area
@@ -50,15 +50,15 @@ include("../include/meta.php");
 				</li>
 			</ul>
      	</div>
-     	<div id="game_workshops" class="workshopcontainer">
+     	<div id="game_badges" class="badgecontainer">
 			<h2>Game</h2>
-			<ul class="workshopList">
+			<ul class="badgeList">
 				<li>
-					<div class="workshopTile"><a href="/tutorials/tutorial.php"><img src="/assets/img/badge_thumb.jpg" /></a></div>
-					<div class="workshopInfo">
+					<div class="badgeTile"><a href="/tutorials/tutorial.php"><img src="/assets/img/badge_thumb.jpg" /></a></div>
+					<div class="badgeInfo">
 						<h2><a href="/tutorials/tutorial.php">Write The Next Angry Birds</a></h2>
 						<a href="/tutorials/tutorial.php">Get this Badge!</p></a>
-						<div class="workshopCareer">
+						<div class="badgeCareer">
 							<p>
 								<span class="careerArea">
 								  Career Area
@@ -69,11 +69,11 @@ include("../include/meta.php");
 					</div>
 				</li>
 				<li>
-					<div class="workshopTile"><a href="/tutorials/tutorial.php"><img src="/assets/img/badge_thumb.jpg" /></a></div>
-					<div class="workshopInfo">
+					<div class="badgeTile"><a href="/tutorials/tutorial.php"><img src="/assets/img/badge_thumb.jpg" /></a></div>
+					<div class="badgeInfo">
 						<h2><a href="/tutorials/tutorial.php">Learn Advanced 3D Rendering <i>and</i> Raytracing</a></h2>
 						<a href="/tutorials/tutorial.php">Get this Badge!</p></a>
-						<div class="workshopCareer">
+						<div class="badgeCareer">
 							<p>
 								<span class="careerArea">
 								  Career Area
@@ -85,15 +85,15 @@ include("../include/meta.php");
 				</li>
 			</ul>
      	</div>
-     	<div id="film_workshops" class="workshopcontainer">
+     	<div id="film_badges" class="badgecontainer">
 			<h2>Film</h2>
-			<ul class="workshopList">
+			<ul class="badgeList">
 				<li>
-					<div class="workshopTile"><a href="/tutorials/tutorial.php"><img src="/assets/img/badge_thumb.jpg" /></a></div>
-					<div class="workshopInfo">
+					<div class="badgeTile"><a href="/tutorials/tutorial.php"><img src="/assets/img/badge_thumb.jpg" /></a></div>
+					<div class="badgeInfo">
 						<h2><a href="/tutorials/tutorial.php">Be The Next Hitchcock</a></h2>
 						<a href="/tutorials/tutorial.php">Get this Badge!</p></a>
-						<div class="workshopCareer">
+						<div class="badgeCareer">
 							<p>
 								<span class="careerArea">
 								  Career Area
@@ -104,11 +104,11 @@ include("../include/meta.php");
 					</div>
 				</li>
 				<li>
-					<div class="workshopTile"><a href="/tutorials/tutorial.php"><img src="/assets/img/badge_thumb.jpg" /></a></div>
-					<div class="workshopInfo">
+					<div class="badgeTile"><a href="/tutorials/tutorial.php"><img src="/assets/img/badge_thumb.jpg" /></a></div>
+					<div class="badgeInfo">
 						<h2><a href="/tutorials/tutorial.php">Learn The Skills of the Gaffer</a></h2>
 						<a href="/tutorials/tutorial.php">Get this Badge!</p></a>
-						<div class="workshopCareer">
+						<div class="badgeCareer">
 							<p>
 								<span class="careerArea">
 								  Career Area
@@ -137,10 +137,10 @@ include("../include/meta.php");
 This is for jsrender, but will easily work with minor tweaks for other engines. -->
 <script type="text/x-jsrender" id="badgeItemTemplate">
 	<li>
-		<div class="workshopTile"><a href="{{:TUTORIAL_LINK}}"><img src="{{:BADGE_IMAGE}}" /></a></div>
-		<div class="workshopInfo">
+		<div class="badgeTile"><a href="{{:TUTORIAL_LINK}}"><img src="{{:BADGE_IMAGE}}" /></a></div>
+		<div class="badgeInfo">
 			<h2><a href="{{:TUTORIAL_LINK}}">{{:TUTORIAL_TITLE}}</a></h2>
-			<div class="workshopCareer">
+			<div class="badgeCareer">
 				<p>
 					<span class="careerArea">
 					  Career Area
